997cbb4407546302690a7015ac63a9781e12b661
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-07-31 15:40:34 +0100

    Creates a save function for album

--- models/album.rb
+++ models/album.rb
@@ -1,8 +1,10 @@
+require_relative("../db/sql_runner.rb")
 require_relative("./artist.rb")
 
 class Album
 
-  attr_reader :id, :name, :genre, :artist_id
+  attr_reader :id, :name, :genre
+  attr_accessor :artist_id
 
   def initialize(album)
     @id = album['id'].to_i() if album['id']
@@ -11,5 +13,18 @@ class Album
     @artist_id = album['artist_id'].to_i()
   end
 
+  def save()
+
+    sql = "INSERT INTO albums
+    (name, genre, artist_id)
+    VALUES ($1, $2, $3) RETURNING id;"
+
+    values = [@name, @genre, @artist_id]
+
+    result = SqlRunner.run(sql, values)
+
+    @id = result[0]['id'].to_i()
+
+  end
 
 end
--- models/artist.rb
+++ models/artist.rb
@@ -2,7 +2,8 @@ require_relative("../db/sql_runner.rb")
 
 class Artist
 
-  attr_reader :id, :name
+  attr_reader :id
+  attr_accessor :name
 
   def initialize(artist)
     @id = artist['id'].to_i() if artist['id']
